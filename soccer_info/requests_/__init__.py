eec57a7ade8e042e9f8ae06bd549de768195f1f8
Ilay <eliyahu.arlev@gmail.com>
2026-01-06 00:03:11 +0200

    chore: revert unrelated file changes

--- soccer_info/requests_/__init__.py
+++ soccer_info/requests_/__init__.py
@@ -1,16 +1,10 @@
 """Request building components for Soccer Football Info API."""
 from .headers import Header
-from .parameters import (
-    BaseParameters,
-    ChampionshipListParameters,
-    ChampionshipViewParameters,
-    ChampionshipBySeasonParameters
-)
+from .parameters import BaseParameters, ChampionshipListParameters, ChampionshipViewParameters
 
 __all__ = [
     'Header',
     'BaseParameters',
     'ChampionshipListParameters', 
     'ChampionshipViewParameters',
-    'ChampionshipBySeasonParameters',
 ]
